d42224d277bef604ba3c320e55ff5c3363be6621
Josh Hess <joshhess13@gmail.com>
2016-12-15 23:04:38 -0700

    Initial Project Setup

---
+++ duo/desktop/src/com/project/duo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.project.duo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.project.duo.Runtime;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Runtime(), config);
+	}
+}
